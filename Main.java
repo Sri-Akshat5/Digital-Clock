650aa98f22d32f8be8b7fa786c265b17c4bd9197
Akshat Srivastava <88766975+Sri-Akshat5@users.noreply.github.com>
2024-02-15 00:33:42 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         MyWindow w = new MyWindow();
-        //This is main file
+        //This is Main file
+        //hi
     }
 }
